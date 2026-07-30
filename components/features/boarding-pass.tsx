236fe2937cecb6df85d1e0081c0b95c6066336d8
Felix Miguel Galpao <felixgalpao04@gmail.com>
2026-07-30 16:27:50 +0800

    Update boarding-pass.tsx

--- components/features/boarding-pass.tsx
+++ components/features/boarding-pass.tsx
@@ -242,7 +242,7 @@ function AirplaneOverlay() {
         delay={CLOUD_ENTRANCE_DELAY}
       />
       <CloudDrift
-        className="absolute right-[-8%] bottom-25 z-0 w-[42%] md:bottom-40"
+        className="absolute right-[2%] bottom-23 z-0 w-[42%] md:bottom-40 md:right-[-8%]"
         offset={16}
         duration={6.5}
         delay={CLOUD_ENTRANCE_DELAY}
